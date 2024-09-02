36f9608195fd9eea7bbb90c92460bfb089fdf4a0
wonchul <kim.wonchul@aiv.ai>
2024-09-02 16:57:11 +0900

    update download-url

--- visionsuite/utils/download.py
+++ visionsuite/utils/download.py
@@ -1,13 +1,19 @@
+import os.path as osp
 import requests
 
 def download_weights_from_url(url, output_filename=None):
     
     try:
+        print(f"Now downloadeding {url}")
         response = requests.get(url)
-        with open(output_filename, 'wb') as file:
-            file.write(response.content)
-
-        print(f"File downloaded and saved as {output_filename}")
+        
+        if not osp.exists(output_filename):
+            with open(output_filename, 'wb') as file:
+                file.write(response.content)
+            print(f"File downloaded and saved as {output_filename}")
+        else:
+            print(f"Already exists at {output_filename}")
+        
     except Exception as error:
         raise RuntimeError(f"[ERROR] There is no such url: {url} - error")
     
