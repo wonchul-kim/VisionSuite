f6b9a9e5a23c41ef7e50ed17f9a6b3a329f8f1c0
wonchul <kim.wonchul@aiv.ai>
2024-10-31 16:58:30 +0900

    refactor dataset

--- visionsuite/engines/classification/src/samplers/default.py
+++ visionsuite/engines/classification/src/samplers/default.py
@@ -4,15 +4,15 @@ from visionsuite.engines.classification.utils.registry import SAMPLERS
 
 
 @SAMPLERS.register()
-def get_samplers(args, dataset, dataset_test):
+def get_samplers(args, train_dataset, val_dataset):
     if args.distributed:
         if hasattr(args, "ra_sampler") and args.ra_sampler:
-            train_sampler = RASampler(dataset, shuffle=True, repetitions=args.ra_reps)
+            train_sampler = RASampler(train_dataset, shuffle=True, repetitions=args.ra_reps)
         else:
-            train_sampler = torch.utils.data.distributed.DistributedSampler(dataset)
-        test_sampler = torch.utils.data.distributed.DistributedSampler(dataset_test, shuffle=False)
+            train_sampler = torch.utils.data.distributed.DistributedSampler(train_dataset)
+        test_sampler = torch.utils.data.distributed.DistributedSampler(val_dataset, shuffle=False)
     else:
-        train_sampler = torch.utils.data.RandomSampler(dataset)
-        test_sampler = torch.utils.data.SequentialSampler(dataset_test)
+        train_sampler = torch.utils.data.RandomSampler(train_dataset)
+        test_sampler = torch.utils.data.SequentialSampler(val_dataset)
         
     return train_sampler, test_sampler
