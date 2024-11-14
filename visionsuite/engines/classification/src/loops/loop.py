7080c74c37ecfcb0a68ed06669fbad05bbadbd01
wonchul <kim.wonchul@aiv.ai>
2024-11-14 14:01:35 +0900

    baseloop changed for cls

--- visionsuite/engines/classification/src/loops/loop.py
+++ visionsuite/engines/classification/src/loops/loop.py
@@ -1,66 +1,20 @@
 import os.path as osp
 from abc import abstractmethod
-from visionsuite.engines.utils.bases.base_oop_module import BaseOOPModule
+
 from visionsuite.engines.classification.src.dataloaders.build import build_dataloader
 from visionsuite.engines.classification.src.losses.build import build_loss
 from visionsuite.engines.classification.src.optimizers.build import build_optimizer
 from visionsuite.engines.classification.src.schedulers.build import build_scheduler
 from visionsuite.engines.classification.src.trainers.build import build_trainer
 from visionsuite.engines.classification.src.validators.build import build_validator
-from visionsuite.engines.utils.torch_utils.utils import load_ckpt
-from visionsuite.engines.classification.utils.registry import LOOPS
+from visionsuite.engines.utils.bases import BaseLoop
 
-class Loop(BaseOOPModule):
+class Loop(BaseLoop):
     
-    required_attributes = ['model', 'train_dataloader', 'lr_scheduler', 'loss', 'optimizer', 'dataset',
-                           'loops', 'start_epoch', 'epochs']
-    
-    def __init__(self, name=None):
-        super().__init__(name=name)
-        self.args = None 
-        
-        self.model = None 
-        self.train_dataloader = None 
-        self.val_dataloader = None 
-        self.lr_scheduler = None 
-        self.loss = None 
-        self.optimizer = None
-        self.scaler = None 
-        
-        self.dataset = None
-        self.archive = None
-        
-        self._start_epoch = None
-        self._epochs = None
-        self.trainer = None 
-        self.validator = None
-        
-        self._loops = []
-        
-    @property 
-    def loops(self):
-        return self._loops 
-    
-    @property 
-    def start_epoch(self):
-        return self._start_epoch 
-    
-    @property 
-    def epochs(self):
-        return self._epochs
-    
-    def set_epoch_for_sampler(self, epoch):
-        if self.args['distributed']['use'] and self.dataset is not None and hasattr('train_sampler', self.dataset) and self.dataset.train_sampler is not None:
-            self.dataset.train_sampler.set_epoch(epoch)
-
     def build(self, _model, _dataset, _archive=None, *args, **kwargs):
-        super().build(*args, **kwargs)
+        super().build(_model=_model, _dataset=_dataset, _archive=_archive, *args, **kwargs)
         self.run_callbacks('on_loop_build_start')
 
-        self.model = _model
-        self.dataset = _dataset
-        self.archive = _archive
-        
         if 'dataloader' in self.args and self.args['dataloader'] is not None:
             self.train_dataloader = build_dataloader(dataset=self.dataset, mode='train', 
                                                     **self.args['dataloader'], 
@@ -118,41 +72,4 @@ class Loop(BaseOOPModule):
         else:
             self.log_warning(f"NO validator", self.build.__name__, __class__.__name__)
             
-    @BaseOOPModule.track_status
-    @abstractmethod
-    def run(self):
-        self.log_info(f"START loop", self.run.__name__, __class__.__name__)  
-        self.run_callbacks('on_loop_run_start')
     
-    def _set_resume(self):
-        if self.args['resume']['use']:
-            assert osp.exists(self.args['resume']['seed_model']), ValueError(f"There is no such checkpoint: {self.args['resume']['seed_model']}")
-            ckpt = load_ckpt(self.args['resume']['seed_model'])
-            self.log_info(f"LOADED ckpt: {ckpt.keys()}", self._set_resume.__name__, __class__.__name__)
-            
-            self.model.model_without_ddp.load_state_dict(ckpt['model'], strict=True)
-            self.log_info(f"LOADED seed_model weights", self._set_resume.__name__, __class__.__name__)
-            
-            attributes = ['optimizer', 'lr_scheduler', 'epoch']
-            if self.args['train']['amp']:
-                attributes.append('scaler')
-            
-            for attribute in attributes:
-                if self.args['resume'][attribute]:
-                    if attribute in ckpt:
-                        if attribute == 'epoch':
-                            self._start_epoch = int(ckpt[attribute]) + 1
-                            if self.args['train']['epochs'] - 10 <= self.start_epoch <= self.args['train']['epochs'] + 10:
-                                self._epochs = self.start_epoch + 100
-                                self.log_info(f"Epochs is updated to {self.start_epoch + 100} since the loaded epoch is {int(ckpt[attribute])}", self._set_resume.__name__, __class__.__name__)
-                        else:
-                            getattr(self, attribute).load_state_dict(ckpt[attribute])
-                    else:
-                        raise AttributeError(f"There is no {attribute} in ckpt({self.args['train']['ckpt']})")
-            self.log_info(f"SET resume lr_scheduler: start_epoch is {self._start_epoch}", self._set_resume.__name__, __class__.__name__) 
-                    
-        else:
-            self._start_epoch = 1
-            self._epochs = self.args['train']['epochs']
-            self.log_info(f"NO resume lr_scheduler: start_epoch is {self._start_epoch}", self._set_resume.__name__, __class__.__name__)   
-            
--- visionsuite/engines/utils/bases/__init__.py
+++ visionsuite/engines/utils/bases/__init__.py
@@ -3,5 +3,6 @@ from .base_train_runner import BaseTrainRunner
 from .base_test_runner import BaseTestRunner
 from .base_oop_module import BaseOOPModule
 from .base_results import BaseResults
+from .base_loop import BaseLoop
 
-__all__ = ['BaseEngine', 'BaseTrainRunner', 'BaseTestRunner', 'BaseOOPModule', 'BaseResults']
+__all__ = ['BaseEngine', 'BaseTrainRunner', 'BaseTestRunner', 'BaseOOPModule', 'BaseResults', 'BaseLoop']
