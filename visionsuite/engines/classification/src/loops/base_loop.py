e1fc6d3fe521cfb414f9132224755bd1dd70bc80
wonchul <kim.wonchul@aiv.ai>
2024-11-14 13:15:58 +0900

    updqte readme

--- visionsuite/engines/classification/src/loops/base_loop.py
+++ visionsuite/engines/classification/src/loops/base_loop.py
@@ -34,6 +34,8 @@ class BaseLoop(BaseOOPModule):
         self.trainer = None 
         self.validator = None
         
+        self.loops = []
+        
     @property 
     def start_epoch(self):
         return self._start_epoch 
@@ -49,30 +51,43 @@ class BaseLoop(BaseOOPModule):
         self.dataset = _dataset
         self.archive = _archive
         
-        self.train_dataloader = build_dataloader(dataset=self.dataset, mode='train', 
-                                                 **self.args['dataloader'], augment=self.args['augment'])
-        self.log_info(f"BUILT train_dataloader: {self.train_dataloader}", self.build.__name__, __class__.__name__)
-        
-        self.val_dataloader = build_dataloader(dataset=self.dataset, mode='val', 
-                                                 **self.args['dataloader'], augment=self.args['augment'])
-        self.log_info(f"BUILT val_dataloader: {self.val_dataloader}", self.build.__name__, __class__.__name__)    
-        
-        self.loss = build_loss(**self.args['loss'])
-        self.log_info(f"BUILT loss: {self.loss}", self.build.__name__, __class__.__name__)      
-        
-        self.optimizer = build_optimizer(model=self.model.model, **self.args['optimizer'])
-        self.log_info(f"BUILT optimizer: {self.optimizer}", self.build.__name__, __class__.__name__)    
-        
-        self.lr_scheduler = build_scheduler(optimizer=self.optimizer, epochs=self.args['train']['epochs'], **self.args['scheduler'])
-        self.log_info(f"BUILT lr_scheduler: {self.lr_scheduler}", self.build.__name__, __class__.__name__)     
-        
-        self._set_resume()
-        self.loop = LOOPS.get(self.args['loop']['type'])
+        if 'dataloader' in self.args and self.args['dataloader'] is not None:
+            self.train_dataloader = build_dataloader(dataset=self.dataset, mode='train', 
+                                                    **self.args['dataloader'], 
+                                                    augment=self.args['augment'] if 'augment' in self.args else None)
+            self.log_info(f"BUILT train_dataloader: {self.train_dataloader}", self.build.__name__, __class__.__name__)
+            
+            self.val_dataloader = build_dataloader(dataset=self.dataset, mode='val', 
+                                                    **self.args['dataloader'], 
+                                                    augment=self.args['augment'] if 'augment' in self.args else None)
+            self.log_info(f"BUILT val_dataloader: {self.val_dataloader}", self.build.__name__, __class__.__name__)    
+        else:
+            self.log_warning(f"NO dataloader", self.build.__name__, __class__.__name__)    
 
+        if 'loss' in self.args and self.args['loss'] is not None:
+            self.loss = build_loss(**self.args['loss'])
+            self.log_info(f"BUILT loss: {self.loss}", self.build.__name__, __class__.__name__)  
+        else:
+            self.log_warning(f"NO loss", self.build.__name__, __class__.__name__)  
+        
+        if 'optimizer' in self.args and self.args['optimizer'] is not None:
+            self.optimizer = build_optimizer(model=self.model.model, **self.args['optimizer'])
+            self.log_info(f"BUILT optimizer: {self.optimizer}", self.build.__name__, __class__.__name__)   
+        else:
+            self.log_warning(f"NO optimizer", self.build.__name__, __class__.__name__)   
+        
+        if 'scheduler' in self.args and self.args['scheduler'] is not None:
+            self.lr_scheduler = build_scheduler(optimizer=self.optimizer, epochs=self.args['train']['epochs'], **self.args['scheduler'])
+            self.log_info(f"BUILT lr_scheduler: {self.lr_scheduler}", self.build.__name__, __class__.__name__)     
+        else:
+            self.log_warning(f"NO lr_scheduler", self.build.__name__, __class__.__name__)     
+
+        self._set_resume()
+            
     @BaseOOPModule.track_status
     @abstractmethod
     def run_loop(self):
-        self.log_info(f"START loop", self._set_resume.__name__, __class__.__name__)  
+        self.log_info(f"START loop", self.run_loop.__name__, __class__.__name__)  
     
     def _set_resume(self):
         if self.args['resume']['use'] and self.args['train']['ckpt']:
