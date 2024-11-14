22fd1907a2cd2630193c04e01f2735d03e244903
wonchul <kim.wonchul@aiv.ai>
2024-11-14 14:17:22 +0900

    base runner is added

--- visionsuite/engines/classification/__init__.py
+++ visionsuite/engines/classification/__init__.py
@@ -6,13 +6,13 @@ class Engine(BaseEngine):
     def __init__(self, task):
         super().__init__(task=task)
         
-    def train(self, cfgs_file, *args, **kwargs):
+    def train(self, cfgs_file=None, *args, **kwargs):
         
         # TODO: need to assign by yaml file
         runner = RUNNERS.get("TrainRunner", case_sensitive=True)('classification')
         assert runner is not None, ValueError(f"runner is None")
         
-        runner.train(cfgs_file, *args, **kwargs)
+        runner.train(cfgs_file=cfgs_file, *args, **kwargs)
                         
         
         
--- visionsuite/engines/segmentation/__init__.py
+++ visionsuite/engines/segmentation/__init__.py
@@ -6,16 +6,16 @@ class Engine(BaseEngine):
     def __init__(self, task):
         super().__init__(task=task)
         
-    def train(self, cfgs_file, *args, **kwargs):
+    def train(self, cfgs_file=None, *args, **kwargs):
         
         runner = RUNNERS.get("TrainRunner", case_sensitive=True)(self.task)
         assert runner is not None, ValueError(f"runner is None")
         
-        runner.train(cfgs_file, *args, **kwargs)
+        runner.train(cfgs_file=cfgs_file, *args, **kwargs)
         
-    def test(self, cfgs_file, *args, **kwargs):
+    def test(self, cfgs_file=None, *args, **kwargs):
         
         runner = RUNNERS.get("TestRunner", case_sensitive=True)(self.task)
         assert runner is not None, ValueError(f"runner is None")
 
-        runner.test(cfgs_file, *args, **kwargs)
+        runner.test(cfgs_file=cfgs_file, *args, **kwargs)
