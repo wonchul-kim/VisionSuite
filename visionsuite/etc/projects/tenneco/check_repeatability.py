ceda7737ded5ff31467c0809369fa326007174cd
wonchul <kim.wonchul@aiv.ai>
2025-03-20 15:26:25 +0900

    update repreatability

--- visionsuite/etc/projects/tenneco/check_repeatability.py
+++ visionsuite/etc/projects/tenneco/check_repeatability.py
@@ -5,13 +5,15 @@ from tqdm import tqdm
 import cv2
 import os
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from shapely.geometry import Polygon
 
 def ond_dim_points_to_polygon(points):
     # points를 (x, y) 형식의 튜플 리스트로 변환
     return [(points[i], points[i + 1]) for i in range(0, len(points), 2)]
 
 def get_polygon_iou(point1, point2):
-    from shapely.geometry import Polygon
     # 두 다각형을 Polygon 객체로 변환
     poly1 = Polygon(ond_dim_points_to_polygon(point1))
     poly2 = Polygon(ond_dim_points_to_polygon(point2))
@@ -33,7 +35,8 @@ def get_polygon_iou(point1, point2):
     
     return iou, poly1.area,poly2.area, intersection_area
 
-def compare_two(anns1, anns2):
+def compare_two(anns1, anns2, iou_threshold, area_threshold=0):
+    
     is_different = False
     for ann1 in anns1:
                 
@@ -59,96 +62,172 @@ def compare_two(anns1, anns2):
             iou, area1, area2, intersection_area = get_polygon_iou(tuple(coord for points in points1 for coord in points), 
                                                                    tuple(coord for points in points2 for coord in points))
 
+            if area_threshold and (area1 < area_threshold and area2 < area_threshold):
+                continue
+
             if iou < iou_threshold:
                 is_different = True 
                 break 
             
         if is_different:
             break 
+        else:
+            poly1 = Polygon(ond_dim_points_to_polygon(tuple(coord for points in points1 for coord in points)))
+            
+            if not poly1.is_valid:
+                poly1 = poly1.buffer(0)
+            
+            if poly1.area < area_threshold:
+                is_different = False 
+            else:
+                is_different = True
+                break
 
     return is_different
 
-base_dir = '/HDD/etc/repeatablility'
-dir_names = ['gcnet_epochs100', 'mask2former_epochs140', 'pidnet_l_epochs300']
-iou_threshold = 0.25
-for dir_name in dir_names:
-    json_files1 = sorted(glob(osp.join(base_dir, dir_name, 'test/exp/labels', '*.json')))
+def run(base_dir, dir_names, iou_thresholds, area_thresholds, vis=False, figs=True):
+       
+    for dir_name in dir_names:
+        results = {}
+        for iou_threshold in iou_thresholds:
+            for area_threshold in area_thresholds:
+                json_files1 = sorted(glob(osp.join(base_dir, dir_name, 'test/exp/labels', '*.json')))
 
-    no_diff_no_points = 0
-    no_diff_points = 0
-    no_diff_points_files = []
-    diff_points = 0
-    
-    for json_file1 in tqdm(json_files1, desc=dir_name):
-        filename = osp.split(osp.splitext(json_file1)[0])[-1]
-        json_file2 = osp.join(base_dir, dir_name, 'test/exp2/labels', f'{filename}.json')
-        json_file3 = osp.join(base_dir, dir_name, 'test/exp3/labels', f'{filename}.json')
-        
-        assert osp.exists(json_file2), ValueError(f"There is no such file: {json_file2}")
-        assert osp.exists(json_file3), ValueError(f"There is no such file: {json_file3}")
-        
-        with open(json_file1, 'r') as jf:
-            anns1 = json.load(jf)['shapes']
-        
-        with open(json_file1, 'r') as jf:
-            anns2 = json.load(jf)['shapes']
-        
-        with open(json_file1, 'r') as jf:
-            anns3 = json.load(jf)['shapes']
-        
-        is_different = False
-        
-        if len(anns1) != len(anns2) or len(anns1) != len(anns3) or len(anns2) != len(anns3):
-            is_different = True 
-        else:
-            if len(anns1) == 0 and len(anns2) == 0 and len(anns3) == 0:
-                no_diff_no_points += 1
-                is_different = False
-            elif compare_two(anns1, anns2) or compare_two(anns1, anns3) or compare_two(anns2, anns1) or compare_two(anns2, anns3) or compare_two(anns3, anns1) or compare_two(anns3, anns2):
-                is_different = True 
-                diff_points += 1
-            else:
-                is_different = False
-                no_diff_points += 1
-                no_diff_points_files.append(filename)
-                        
-        if is_different:
-            img1 = cv2.imread(osp.join(base_dir, dir_name, f'test/exp/vis/{filename}_3_0.png'))
-            img2 = cv2.imread(osp.join(base_dir, dir_name, f'test/exp2/vis/{filename}_3_0.png'))
-            img3 = cv2.imread(osp.join(base_dir, dir_name, f'test/exp3/vis/{filename}_3_0.png'))
-            
-            diff_dir = osp.join(base_dir, dir_name, 'diff_w_points')
-            if not osp.exists(diff_dir):
-                os.mkdir(diff_dir)
-            
-            cv2.imwrite(osp.join(diff_dir, f'{filename}.png'), np.vstack([img1, img2, img3]))
-        
-                        
-        if filename in no_diff_points_files:
-            img1 = cv2.imread(osp.join(base_dir, dir_name, f'test/exp/vis/{filename}_3_0.png'))
-            img2 = cv2.imread(osp.join(base_dir, dir_name, f'test/exp2/vis/{filename}_3_0.png'))
-            img3 = cv2.imread(osp.join(base_dir, dir_name, f'test/exp3/vis/{filename}_3_0.png'))
-            
-            no_diff_dir = osp.join(base_dir, dir_name, 'no_diff_w_points')
-            if not osp.exists(no_diff_dir):
-                os.mkdir(no_diff_dir)
-            
-            cv2.imwrite(osp.join(no_diff_dir, f'{filename}.png'), np.vstack([img1, img2, img3]))
-        
-            
-    assert len(json_files1) == no_diff_no_points + no_diff_points + diff_points
-            
-    txt = open(osp.join(base_dir, dir_name, 'diff.txt'), 'w')
-    txt.write(f"No diff. b/c no points: {no_diff_no_points}\n")
-    txt.write(f"No diff. even with points: {no_diff_points}\n")
-    txt.write(f"diff. with points: {diff_points}\n")
-    txt.close()
-        
-            
-            
+                no_diff_no_points = 0
+                no_diff_points = 0
+                no_diff_points_files = []
+                diff_points = 0
                 
-        
+                for json_file1 in tqdm(json_files1, desc=f"{dir_name}: IoU({iou_threshold}) & Area({area_threshold})"):
+                    filename = osp.split(osp.splitext(json_file1)[0])[-1]
+                    
+                    json_file2 = osp.join(base_dir, dir_name, 'test/exp2/labels', f'{filename}.json')
+                    json_file3 = osp.join(base_dir, dir_name, 'test/exp3/labels', f'{filename}.json')
+                    
+                    assert osp.exists(json_file2), ValueError(f"There is no such file: {json_file2}")
+                    assert osp.exists(json_file3), ValueError(f"There is no such file: {json_file3}")
+                    
+                    with open(json_file1, 'r') as jf:
+                        anns1 = json.load(jf)['shapes']
+                    
+                    with open(json_file2, 'r') as jf:
+                        anns2 = json.load(jf)['shapes']
+                    
+                    with open(json_file3, 'r') as jf:
+                        anns3 = json.load(jf)['shapes']
+                    
+                    is_different = False
+                    if len(anns1) == 0 and len(anns2) == 0 and len(anns3) == 0:
+                        # all cases are no points
+                        no_diff_no_points += 1
+                        is_different = False
+                    elif compare_two(anns1, anns2, iou_threshold, area_threshold) or compare_two(anns1, anns3, iou_threshold, area_threshold) or compare_two(anns2, anns1, iou_threshold, area_threshold) or compare_two(anns2, anns3, iou_threshold, area_threshold) or compare_two(anns3, anns1, iou_threshold, area_threshold) or compare_two(anns3, anns2, iou_threshold, area_threshold):
+                        # different predicted points
+                        is_different = True 
+                        diff_points += 1
+                    else:
+                        # no difference even with predicted points
+                        is_different = False
+                        no_diff_points += 1
+                        no_diff_points_files.append(filename)
+                    
+                    if vis and is_different:
+                        img1 = cv2.imread(osp.join(base_dir, dir_name, f'test/exp/vis/{filename}_3_0.png'))
+                        img2 = cv2.imread(osp.join(base_dir, dir_name, f'test/exp2/vis/{filename}_3_0.png'))
+                        img3 = cv2.imread(osp.join(base_dir, dir_name, f'test/exp3/vis/{filename}_3_0.png'))
                         
+                        diff_dir = osp.join(base_dir, dir_name, f'iou{iou_threshold}_area{area_threshold}', 'diff_w_points')
+                        if not osp.exists(diff_dir):
+                            os.makedirs(diff_dir)
+                        
+                        cv2.imwrite(osp.join(diff_dir, f'{filename}.png'), np.vstack([img1, img2, img3]))
+                    
+                                    
+                    if vis and filename in no_diff_points_files:
+                        img1 = cv2.imread(osp.join(base_dir, dir_name, f'test/exp/vis/{filename}_3_0.png'))
+                        img2 = cv2.imread(osp.join(base_dir, dir_name, f'test/exp2/vis/{filename}_3_0.png'))
+                        img3 = cv2.imread(osp.join(base_dir, dir_name, f'test/exp3/vis/{filename}_3_0.png'))
+                        
+                        no_diff_dir = osp.join(base_dir, dir_name,  f'iou{iou_threshold}_area{area_threshold}', 'no_diff_w_points')
+                        if not osp.exists(no_diff_dir):
+                            os.makedirs(no_diff_dir)
+                        
+                        cv2.imwrite(osp.join(no_diff_dir, f'{filename}.png'), np.vstack([img1, img2, img3]))
+                    
+                        
+                assert len(json_files1) == no_diff_no_points + no_diff_points + diff_points, ValueError(f"{len(json_files1)} == {no_diff_no_points} + {no_diff_points} + {diff_points}")
+                        
+                results[f'iou{iou_threshold}_area{area_threshold}'] = {'no_diff_no_points': no_diff_no_points, 'no_diff_points': no_diff_points, 'diff_points': diff_points}
+                
+                if figs:
+                    txt = open(osp.join(base_dir, dir_name, 'diff.txt'), 'a')
+                    txt.write(f"\n >>>>>>>>>>>>>>>> IoU Threshold: {iou_threshold} & Area Threshold: {area_threshold} <<<<<<<<<<<<<<<<\n")
+                    txt.write(f"No diff. b/c no points: {no_diff_no_points}\n")
+                    txt.write(f"No diff. even with points: {no_diff_points}\n")
+                    txt.write(f"diff. with points: {diff_points}\n")
+                    txt.write("============================================================================\n")
+                    txt.close()
+
+        if figs:
+            df = pd.DataFrame.from_dict(results)     
+            df.to_csv(osp.join(base_dir, dir_name, 'diff.csv'))
+            
+            # X축 레이블
+            categories = list(results.keys())
+
+            # Y축 값 분리
+            no_diff_no_points = [results[key]['no_diff_no_points'] for key in categories]
+            no_diff_points = [results[key]['no_diff_points'] for key in categories]
+            diff_points = [results[key]['diff_points'] for key in categories]
+
+            fig, axes = plt.subplots(3, 1, figsize=(12, 12), sharex=True)
+
+            # 라인 그래프 설정
+            colors = ['blue', 'orange', 'green']
+            labels = ['no_diff_no_points', 'no_diff_points', 'diff_points']
+            data = [no_diff_no_points, no_diff_points, diff_points]
+
+            # 각 서브플롯에 데이터 추가
+            for i, ax in enumerate(axes):
+                ax.plot(categories, data[i], marker='o', color=colors[i], linestyle='-', label=labels[i])
+                ax.set_ylabel('Count')
+                ax.set_title(labels[i])
+                ax.legend()
+                ax.grid(axis='y', linestyle='--', alpha=0.7)
+                
+                # 데이터 포인트 값 표시
+                for j, txt in enumerate(data[i]):
+                    ax.text(j, txt, str(txt), ha='center', va='bottom', fontsize=10)
+
+            # X축 설정
+            axes[-1].set_xticks(np.arange(len(categories)))
+            axes[-1].set_xticklabels(categories, rotation=45, ha='right')
+
+            # 전체 제목 추가
+            fig.suptitle('IoU & Area Settings - Line Plot Comparison', fontsize=14)
+            plt.xlabel('IoU & Area Settings')
+
+            # 그래프 출력
+            plt.tight_layout(rect=[0, 0, 1, 0.97])
+            plt.savefig(osp.join(base_dir, dir_name, 'diff.png'))
+            
+if __name__ == '__main__':
+
+    base_dir = '/HDD/etc/repeatablility'
+    dir_names = ['gcnet_epochs100', 'mask2former_epochs140', 'pidnet_l_epochs300', 'sam2unet_epochs100']
+    # dir_names = ['gcnet_epochs100']
+    # iou_thresholds = [0.05, 0.1, 0.2]
+    # area_thresholds = [10, 50, 100]
+    # figs = True 
+    # vis = False
+    iou_thresholds = [0.05]
+    area_thresholds = [100]
+    vis = True
+    figs = False
+                   
+    run(base_dir, dir_names, iou_thresholds, area_thresholds, vis, figs)
+            
+                            
             
                 
                     
