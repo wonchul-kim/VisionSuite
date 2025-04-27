9c9950323306646a923bef42e821cd1070a7b315
wonchul <kim.wonchul@aiv.ai>
2025-04-27 23:00:25 +0900

    update

--- visionsuite/etc/projects/compare_trt_python.py
+++ visionsuite/etc/projects/compare_trt_python.py
@@ -153,7 +153,7 @@ def vis_pred(trt_input_dir, python_input_dir, _output_dir, filename):
 
     plt.suptitle(f"trt (left) vs. python (right)", fontsize=14)
     plt.tight_layout()
-    plt.savefig(osp.join(_output_dir, filename + '_pred.png'))
+    plt.savefig(osp.join(_output_dir, filename + '_pred.jpg'))
     plt.close()
     
 def vis_historgram_by_channel(trt_arr, python_arr, is_same, rtol, atol, _output_dir, filename):
@@ -198,27 +198,145 @@ def vis_historgram_by_channel(trt_arr, python_arr, is_same, rtol, atol, _output_
     plt.savefig(osp.join(_output_dir, filename + '_histo.jpg'))
     plt.close()
 
+def get_case(trt_arr, python_arr, filename, contour_confidence_threshold, contour_threshold, case_iou_threshold, case_iou_rect, case_offset, 
+                result, ious, case_areas_diff, count):
+    case = -1
+    
+    if case == 3:
+        return case
+    
+    trt_blobs, python_blobs = {}, {}
+    for channel in range(1, trt_arr.shape[-1]):
+        trt_arr[trt_arr < contour_confidence_threshold] = 0
+        python_arr[python_arr < contour_confidence_threshold] = 0
+        
+        image = trt_arr[..., channel].astype(np.uint8)
+        t_blobs, t_areas = get_blobs(image, contour_thres=contour_threshold)
+        if t_areas != []:
+            trt_blobs[channel] = {'blobs': t_blobs, 'mean area': np.mean(t_areas), 'min area': np.min(t_areas), 'max area': np.max(t_areas), 'std area': np.std(t_areas), 'count': len(t_blobs)}
+        else:
+            trt_blobs[channel] = {'blobs': t_blobs, 'mean area': None, 'min area': None, 'max area': None, 'std area': None}
+        image = python_arr[..., channel].astype(np.uint8)
+        p_blobs, p_areas = get_blobs(image, contour_thres=contour_threshold)
+        if p_areas != []:
+            python_blobs[channel] = {'blobs': p_blobs, 'mean area': np.mean(p_areas), 'min area': np.min(p_areas), 'max area': np.max(p_areas), 'std area': np.std(p_areas), 'count': len(p_blobs)}
+        else:
+            python_blobs[channel] = {'blobs': p_blobs, 'mean area': None, 'min area': None, 'max area': None, 'std area': None}
+
+
+        if len(trt_blobs[channel]['blobs']) == 0 and len(python_blobs[channel]['blobs']) == 0:
+            case = 1
+        elif (len(trt_blobs[channel]['blobs']) == 0 and len(python_blobs[channel]['blobs']) != 0) or (len(trt_blobs[channel]['blobs']) != 0 and len(python_blobs[channel]['blobs']) == 0):
+            case = 3
+            
+            if len(trt_blobs[channel]['blobs']) ==0:
+                blobs = python_blobs[channel]['blobs']
+            else:
+                blobs = trt_blobs[channel]['blobs']
+                
+            for blob in blobs:
+                if case_iou_rect:
+                    points = polygon2rect(blob['polygon'], case_offset)
+                else:
+                    points = blob['polygon']
+                
+                if len(points) <= 2:
+                    area = 1
+                else:
+                    poly = Polygon(ond_dim_points_to_polygon(tuple(coord for points in points for coord in points)))
+                    
+                    if not poly.is_valid:
+                        poly = poly.buffer(0)
+                    area = poly.area
+                    
+                case_areas_diff[f'case{case}'].append(area)
+        else:
+            if len(trt_blobs[channel]['blobs']) > len(python_blobs[channel]['blobs']):
+                blobs_1 = trt_blobs[channel]['blobs']
+                blobs_2 = python_blobs[channel]['blobs']
+            else:
+                blobs_2 = trt_blobs[channel]['blobs']
+                blobs_1 = python_blobs[channel]['blobs']
+                
+            for trt_blob in blobs_1:
+                for python_blob in blobs_2:
+                    
+                    if len(trt_blob['polygon']) <= 2 or len(python_blob['polygon']) <= 2:
+                        iou = 0
+                        
+                        if len(trt_blob['polygon']) <= 2:
+                            area1 = 1
+                        else:
+                            poly = Polygon(ond_dim_points_to_polygon(tuple(coord for points in trt_blob['polygon'] for coord in points)))
+                            
+                            if not poly.is_valid:
+                                poly = poly.buffer(0)
+                            area1 = poly.area
+                        
+                        if len(python_blob['polygon']) <= 2:
+                            area2 = 1
+                        else:
+                            poly = Polygon(ond_dim_points_to_polygon(tuple(coord for points in python_blob['polygon'] for coord in points)))
+                            
+                            if not poly.is_valid:
+                                poly = poly.buffer(0)
+                            area2 = poly.area
+                        
+                    else:
+                        if case_iou_rect:
+                            trt_points = polygon2rect(trt_blob['polygon'], case_offset)
+                            python_points = polygon2rect(python_blob['polygon'], case_offset)
+                        else:
+                            trt_points = trt_blob['polygon']
+                            python_points = python_blob['polygon']
+
+                        iou, area1, area2, intersection_area = get_polygon_iou(tuple(coord for points in trt_points for coord in points), 
+                                                                                tuple(coord for points in python_points for coord in points))
+                    ious.append(iou)
+                    if iou > case_iou_threshold:
+                        case = 2
+                        case_areas_diff[f'case{case}'].append(abs(area1 - area2))
+                        break
+                
+            if case == 3 or case == 1:
+                case = 3
+                case_areas_diff[f'case{case}'].append(abs(area1 - area2))
+            # elif case == 1:
+            #     case = 3
+                    
+    assert case != -1, RuntimeError(f"Case cannot be -1")
+    
+    count[case] += 1
+    result[filename] = {'trt': trt_blobs, 'python': python_blobs, 'case': case}
+        
+    return case
+
+    
 
 def main():
     height, width, num_classes = 768, 1120, 4
     rtol = 1e-2
     atol = 1e-2    
-    contour_threshold = 10
-    contour_confidence_threshold = 0.1
-    case_iou_threshold = 0.1
-    case_offset = 30
+    contour_threshold = 1
+    contour_confidence_threshold = 0.05
+    case_iou_threshold = 0
+    case_offset = 0
+    case_iou_rect = False
 
     num_same, num_not_same = 0, 0
     trt_input_dir = f'/DeepLearning/etc/_athena_tests/benchmark/trt_vs_python/trt/1'       
     python_input_dir = f'/DeepLearning/etc/_athena_tests/benchmark/trt_vs_python/test/exp/vis/raw'
-    output_dir = f'/DeepLearning/etc/_athena_tests/benchmark/trt_vs_python/compare/rtol{rtol}_atol{atol}'
+    # output_dir = f'/DeepLearning/etc/_athena_tests/benchmark/trt_vs_python/compare/rtol{rtol}_atol{atol}'
+    output_dir = f'/HDD/etc/python_vs_trt/compare/rtol{rtol}_atol{atol}'
 
     if not osp.exists(output_dir):
         os.makedirs(output_dir, exist_ok=True)
         
     bin_files = glob(osp.join(trt_input_dir, '*.bin'))
-    # bin_files = glob(osp.join(trt_input_dir, '125020717055621_14_Outer_1.bmp.bmp.bin'))
-
+    # bin_files = glob(osp.join(trt_input_dir, '125032816414391_4.bmp.bmp.bin'))
+    # bin_files = glob(osp.join(trt_input_dir, '125032816410911_9.bmp.bmp.bin'))
+    # bin_files = glob(osp.join(trt_input_dir, '125032816411208_8.bmp.bmp.bin'))
+    # bin_files = glob(osp.join(trt_input_dir, '125032816551971_3.bmp.bmp.bin'))
     result = {}
     '''
         result = { "image filename": {
@@ -270,8 +388,13 @@ def main():
         }
     '''
     idx = 0
-    ious, trt_areas, python_areas = [], [], []
+    ious = []
+    case_areas_diff = {'case2': [], 'case3': []}
+    count = {1: 0, 2: 0, 3: 0}
+    filenames = {1: [], 2: [], 3: []}
     for bin_file in tqdm(bin_files):
+    # for idx in range(len(bin_files)):
+    #     bin_file = bin_files[1197 + idx]
         idx += 1
         filename = osp.split(osp.splitext(bin_file)[0])[-1].split(".")[0]
         npz_file = osp.join(python_input_dir, filename + '.npz')
@@ -300,112 +423,137 @@ def main():
         if not osp.exists(_output_dir):
             os.mkdir(_output_dir)
         
-        # vis_raw_output_by_channel(trt_arr, python_arr, _output_dir, filename)
-        # vis_pred(trt_input_dir, python_input_dir, _output_dir, filename)
-        # vis_historgram_by_channel(trt_arr, python_arr, is_same, rtol, atol, _output_dir, filename)
+        case = get_case(trt_arr, python_arr, filename, contour_confidence_threshold, contour_threshold, case_iou_threshold, case_iou_rect, case_offset, 
+                result, ious, case_areas_diff, count)
+        
+        filenames[case].append(filename)
+        _output_dir = osp.join(_output_dir, f"case{str(case)}")
+        if not osp.exists(_output_dir):
+            os.mkdir(_output_dir)
+        
+        vis_raw_output_by_channel(trt_arr, python_arr, _output_dir, filename)
+        vis_pred(trt_input_dir, python_input_dir, _output_dir, filename)
+        vis_historgram_by_channel(trt_arr, python_arr, is_same, rtol, atol, _output_dir, filename)
 
-        case = -1
-        trt_blobs, python_blobs = {}, {}
-        for channel in range(1, trt_arr.shape[-1]):
-            trt_arr[trt_arr < contour_confidence_threshold] = 0
-            python_arr[python_arr < contour_confidence_threshold] = 0
+
+        if idx%100 == 0:
+            fig = plt.figure(figsize=(20, 10))
+            plt.hist(ious, bins=50, color='skyblue', edgecolor='black')
+            plt.title('Distribution of IoUs')
+            plt.xlabel('IoU')
+            plt.ylabel('Count')    
+            plt.savefig(osp.join(output_dir, 'ious.jpg'))
+            plt.close()
+
+            fig = plt.figure(figsize=(20, 10))
+            plt.boxplot(case_areas_diff.values(), labels=case_areas_diff.keys())
+            plt.title("Area difference for each case")
+            plt.ylabel("Area")
+            plt.savefig(osp.join(output_dir, 'case_areas_diff.jpg'))
+            plt.close()
             
-            image = trt_arr[..., channel].astype(np.uint8)
-            t_blobs, t_areas = get_blobs(image, contour_thres=contour_threshold)
-            trt_areas += t_areas
-            if t_areas != []:
-                trt_blobs[channel] = {'blobs': t_blobs, 'mean area': np.mean(t_areas), 'min area': np.min(t_areas), 'max area': np.max(t_areas), 'std area': np.std(t_areas), 'count': len(t_blobs)}
-            else:
-                trt_blobs[channel] = {'blobs': t_blobs, 'mean area': None, 'min area': None, 'max area': None, 'std area': None}
-            image = python_arr[..., channel].astype(np.uint8)
-            p_blobs, p_areas = get_blobs(image, contour_thres=contour_threshold)
-            python_areas += p_areas
-            if p_areas != []:
-                python_blobs[channel] = {'blobs': p_blobs, 'mean area': np.mean(p_areas), 'min area': np.min(p_areas), 'max area': np.max(p_areas), 'std area': np.std(p_areas), 'count': len(p_blobs)}
-            else:
-                python_blobs[channel] = {'blobs': p_blobs, 'mean area': None, 'min area': None, 'max area': None, 'std area': None}
+            fig, axes = plt.subplots(1, 2, figsize=(20, 10), sharey=True)
+            axes[0].hist(case_areas_diff['case2'], bins=50, color='skyblue', edgecolor='black')
+            axes[0].set_title('Case2 Areas')
+            axes[0].set_xlabel('Area')
+            axes[0].set_ylabel('Count')
 
+            axes[1].hist(case_areas_diff['case3'], bins=50, color='salmon', edgecolor='black')
+            axes[1].set_title('Case3 Areas')
+            axes[1].set_xlabel('Area')
 
-            if len(trt_blobs[channel]['blobs']) == 0 and len(python_blobs[channel]['blobs']) == 0:
-                case = 1
-            elif (len(trt_blobs[channel]['blobs']) == 0 and len(python_blobs[channel]['blobs']) != 0) or (len(trt_blobs[channel]['blobs']) != 0 and len(python_blobs[channel]['blobs']) == 0):
-                case = 3
-            else:
+            plt.tight_layout()
+            plt.savefig(osp.join(output_dir, 'case_areas_histogram.jpg'))
+            plt.close()
+
+            
+            fig = plt.figure(figsize=(20, 10))
+            plt.bar(count.keys(), count.values())
+            plt.xlabel("Key")
+            plt.ylabel("Value")
+            plt.title("The number of each case")
+            plt.savefig(osp.join(output_dir, 'count_case.jpg'))
+            plt.close()
+
+            with open(osp.join(output_dir, 'result.json'), 'w') as jf:
+                json.dump(result, jf, indent=4)
                 
-                for trt_blob in trt_blobs[channel]['blobs']:
-                    for python_blob in python_blobs[channel]['blobs']:
-                        
-                        iou, area1, area2, intersection_area = get_polygon_iou(tuple(coord for points in polygon2rect(trt_blob['polygon'], case_offset) for coord in points), 
-                                              tuple(coord for points in polygon2rect(python_blob['polygon'], case_offset) for coord in points))
-                        ious.append(iou)
-                        if iou > case_iou_threshold:
-                            case = 2
-                        else:
-                            case = 3
-                            break
-                        
-        result[filename] = {'trt': trt_blobs, 'python': python_blobs, 'case': case}
-        
-        if idx > 3:
-            break
+            with open(osp.join(output_dir, 'result.pkl'), "wb") as f:
+                pickle.dump(result, f)
+
+            rows = []
+
+            for img_name, img_data in result.items():
+                for method in ['trt', 'python']:
+                    for channel in range(1, num_classes):
+                        blob_data = img_data[method][channel]
+                        row = {
+                            'image': img_name,
+                            'method': method,
+                            'channel': channel,
+                            'mean_area': blob_data['mean area'],
+                            'min_area': blob_data['min area'],
+                            'max_area': blob_data['max area'],
+                            'std_area': blob_data['std area'],
+                            'case': img_data['case'],
+                        }
+                        rows.append(row)
+
+            df = pd.DataFrame(rows)
+            df.to_csv(osp.join(output_dir, "results.csv"), index=False)
+
+
+            num_txt = open(osp.join(output_dir, 'numbers.txt'), 'w')
+            num_txt.write(f'number of same: {num_same}\n')
+            num_txt.write(f'number of NOT same: {num_not_same}\n')
+            num_txt.close()
+
+            for key, val in filenames.items():
+                filename_txt = open(osp.join(output_dir, f'case{key}_filenames.txt'), 'w')
+                for filename in val:
+                    filename_txt.write(f'{filename}\n')
+                filename_txt.close()
+
+
+def resummary(all_data):
     
-
-    fig = plt.figure(figsize=(20, 20))
-    plt.hist(ious, bins=20, color='skyblue', edgecolor='black')
-    plt.title('Distribution of IoUs')
+    case_areas = {'case2': [], 'case3': []}
+    count = {1: 0, 2: 0, 3: 0}
+    for filename, data in all_data.items():
+        count[data['case']] += 1
+        
+        if data['case'] == 1:
+            continue
+        for defect_index in ['1', '2', '3']:
+            python_max_area = data['python']['1']['max area']
+            trt_max_area = data['trt']['1']['max area']
+            
+            if python_max_area is not None:
+                case_areas[f"case{data['case']}"].append(python_max_area)
+                
+            if trt_max_area is not None:
+                case_areas[f"case{data['case']}"].append(trt_max_area)
+                
+    fig = plt.figure(figsize=(20, 10))
+    plt.hist(case_areas['case2'], bins=100, color='skyblue', edgecolor='black')
+    plt.title('Distribution of area for case2')
     plt.xlabel('IoU')
     plt.ylabel('Count')    
-    plt.savefig(osp.join(output_dir, 'ious.jpg'))
+    plt.savefig(osp.join('/HDD/etc/python_vs_trt/compare/rtol0.01_atol0.01/', 'case2_areas.jpg'))
     plt.close()
-
-    fig = plt.figure(figsize=(20, 20))
-    plt.hist(trt_areas, bins=20, color='skyblue', edgecolor='black')
-    plt.title('Distribution of trt blob areas')
+        
+    fig = plt.figure(figsize=(20, 10))
+    plt.hist(case_areas['case3'], bins=100, color='skyblue', edgecolor='black')
+    plt.title('Distribution of area for case3')
     plt.xlabel('IoU')
     plt.ylabel('Count')    
-    plt.savefig(osp.join(output_dir, 'trt_areas.jpg'))
+    plt.savefig(osp.join('/HDD/etc/python_vs_trt/compare/rtol0.01_atol0.01/', 'case3_areas.jpg'))
     plt.close()
     
-    fig = plt.figure(figsize=(20, 20))
-    plt.hist(python_areas, bins=20, color='skyblue', edgecolor='black')
-    plt.title('Distribution of python blob areas')
-    plt.xlabel('IoU')
-    plt.ylabel('Count')    
-    plt.savefig(osp.join(output_dir, 'python_areas.jpg'))
-    plt.close()
-
-    with open(osp.join(output_dir, 'result.json'), 'w') as jf:
-        json.dump(result, jf, indent=4)
-        
-    with open(osp.join(output_dir, 'result.pkl'), "wb") as f:
-        pickle.dump(result, f)
-
-    rows = []
-
-    for img_name, img_data in result.items():
-        for method in ['trt', 'python']:
-            for channel in range(1, num_classes):
-                blob_data = img_data[method][channel]
-                row = {
-                    'image': img_name,
-                    'method': method,
-                    'channel': channel,
-                    'mean_area': blob_data['mean area'],
-                    'min_area': blob_data['min area'],
-                    'max_area': blob_data['max area'],
-                    'std_area': blob_data['std area'],
-                    'case': img_data['case'],
-                }
-                rows.append(row)
-
-    df = pd.DataFrame(rows)
-    df.to_csv(osp.join(output_dir, "results.csv"), index=False)
-
-    num_txt = open(osp.join(output_dir, 'numbers.txt'), 'w')
-    num_txt.write(f'number of same: {num_same}\n')
-    num_txt.write(f'number of NOT same: {num_not_same}\n')
-    num_txt.close()
-
-
 if __name__ == '__main__':
     main()
+    
+    # with open('/HDD/etc/python_vs_trt/compare/rtol0.01_atol0.01/result.json', 'r') as jf:
+    #     all_data = json.load(jf)
+        
+    # resummary(all_data)
--- visionsuite/etc/projects/tenneco/make_repeatability_test_dataset.py
+++ visionsuite/etc/projects/tenneco/make_repeatability_test_dataset.py
@@ -3,21 +3,29 @@ import os
 import shutil 
 from pathlib import Path
 from tqdm import tqdm
+from glob import glob 
 
 
-
-def make_repeatability_test_dataset(input_dir, output_dir):
+def make_repeatability_test_dataset(input_dir, output_dir, filenames_dir):
+    
+    filenames = glob(osp.join(filenames_dir, "*.png"))
+    _filenames = []
+    for filename in filenames:
+        filename = osp.split(osp.splitext(filename)[0])[-1]
+        _filenames.append(filename)
+        
+    filenames = _filenames
+    
     if not osp.exists(output_dir):
         os.mkdir(output_dir)
 
-    cases = [1, 2, 3]
-    fovs = list(range(1, 15))
+    cases = [1]
     for case in cases:
         base_dir = osp.join(input_dir, str(case))
         folders = sorted([Path(base_dir) / f for f in os.listdir(base_dir) if (Path(base_dir) / f).is_dir()])
         # sorted_folders = sorted(folders, key=lambda f: f.stat().st_ctime)
         print(f"There are {len(folders)} folders")
-
+        
         _output_dir = osp.join(output_dir, str(case))
         
         if case == 1:
@@ -28,23 +36,17 @@ def make_repeatability_test_dataset(input_dir, output_dir):
 
         for idx, folder_dir in tqdm(enumerate(folders), desc=f"case: {str(case)}"):
             folder_dir = str(folder_dir)
-            for fov in fovs:
-                img_file = osp.join(folder_dir, f"{fov}_Outer.bmp")
+            filename = folder_dir.split('/')[-1]
+            
+            if filename in filenames:
+                img_file = osp.join(folder_dir, f"1_image.bmp")
                 assert osp.exists(img_file), ValueError(f"There is no such image file: {img_file}")
                 
-                __output_dir = osp.join(_output_dir, str(sorted_folders_case_1[idx]).split("/")[-1] + f'_{fov}')
-                
-                if not osp.exists(__output_dir):
-                    os.mkdir(__output_dir)
-                
-                shutil.copyfile(img_file, osp.join(__output_dir, '1_image.bmp'))
+                shutil.copyfile(img_file, osp.join(output_dir, filename + '.bmp'))
                 
 
-
-                
-                
-                
 if __name__ == '__main__':                
-    input_dir = '/Data/01.Image/Tenneco/Metalbearing/4_FOR_SIMULATION/TEST_SET/benchmark/반복성/250328/PC2'
-    output_dir = '/HDD/datasets/projects/Tenneco/Metalbearing/outer/repeatability'
-    make_repeatability_test_dataset(input_dir, output_dir)
+    input_dir = '/DeepLearning/etc/_athena_tests/benchmark/tenneco/outer_repeat_unit/datasets'
+    filenames_dir = '/DeepLearning/etc/_athena_tests/benchmark/tenneco/outer_repeat_unit/segmentation/deeplabv3plus/wo_patch/no_tta/exp/미..검'
+    output_dir = '/DeepLearning/etc/_athena_tests/benchmark/tenneco/performances/dataset'
+    make_repeatability_test_dataset(input_dir, output_dir, filenames_dir)
